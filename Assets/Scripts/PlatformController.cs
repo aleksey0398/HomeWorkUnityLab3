e6962370fad49751a1a67970abbee124fd554858
Alexey.Mitkin <alexey.mitkin@forextime.com>
2020-04-28 15:47:35 +0300

    Added falling for platform

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private Transform transform;
+    private Animator animator;
+
+    private bool platfromIsFalling = false;
+
+    private const string ANIMATION_NAME_SHAKE = "Shake";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        transform = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+
+        animator.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void PlatformMoving()
+    {
+        rigidbody.isKinematic = true;
+
+        float x = UnityEngine.Random.Range(-3f, 4f);
+        float z = UnityEngine.Random.Range(-5f, 3f);
+        float y = UnityEngine.Random.Range(2f, 4f);
+
+        Vector3 newVector = new Vector3(x, y, z);
+
+        transform.position = newVector;
+        transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+
+        platfromIsFalling = false;
+    }
+
+    private void StartAnimationBeforeFalling()
+    {
+        platfromIsFalling = true;
+
+        animator.enabled = true;
+        StartCoroutine(ExecuteAfterTime(1.5f, PlatformFalling));
+    }
+
+    private void PlatformFalling()
+    {
+        animator.enabled = false;
+        rigidbody.isKinematic = false;
+        StartCoroutine(ExecuteAfterTime(1.3f, PlatformMoving));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!platfromIsFalling)
+            StartAnimationBeforeFalling();
+    }
+
+    IEnumerator ExecuteAfterTime(float time, Action task)
+    {
+        yield return new WaitForSeconds(time);
+        task();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
     Rigidbody rigidbody;
     Animator animator;
     bool isGround = false;
-    bool doubleJump = true;
 
     // Start is called before the first frame update
     void Start()
@@ -25,36 +24,21 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space) && isGround)
         {
-            rigidbody.AddForce(new Vector3(0, 1000, 0));
+            rigidbody.AddForce(new Vector3(0, 1700, 0));
             animator.Play("PlayerJump", -1, 0f);
-            doubleJump = true;
             
-        } else if(Input.GetKeyDown(KeyCode.Space) && doubleJump)
-        {
-            doubleJump = false;
-            rigidbody.AddForce(new Vector3(0, 1500, 0));
-            animator.Play("PlayerJumpDouble", -1, 0f);
-
-        }
+        } 
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         isGround = true;
- 
-        Debug.Log("On enter: "+collision.gameObject.tag);
 
     }
 
     private void OnCollisionExit(Collision collision)
     {
         isGround = false;
-        Debug.Log("On exit: "+collision.gameObject.tag);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-   
-    }
 }
